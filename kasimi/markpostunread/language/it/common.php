75d2633a6ccfecbb7bcdedd4fc5035aa1ed54b96
Mauron <Mauron@users.noreply.github.com>
2015-12-07 18:05:14 +0100

    Proper translation for “topic”

--- kasimi/markpostunread/language/it/common.php
+++ kasimi/markpostunread/language/it/common.php
@@ -26,10 +26,10 @@ $lang = array_merge($lang, array(
 
 	// Navbar
 	'MARKPOSTUNREAD_UNREAD_NONE'			=> 'Non ci sono messaggi non letti',
-	'MARKPOSTUNREAD_UNREAD_NUM_MAX'			=> 'Ci sono messaggi non letti in più di %1$d topic',
+	'MARKPOSTUNREAD_UNREAD_NUM_MAX'			=> 'Ci sono messaggi non letti in più di %1$d argomenti',
 	'MARKPOSTUNREAD_UNREAD_NUM'				=> array(
 		0 => 'Non ci sono messaggi non letti',
-		1 => 'Messaggi non letti in %1$d topic',
-		2 => 'Messaggi non letti in %1$d topic', // in Italian, the plural form of “topic” is still “topic”
+		1 => 'Messaggi non letti in %1$d argomento',
+		2 => 'Messaggi non letti in %1$d argomenti',
 	),
 ));
