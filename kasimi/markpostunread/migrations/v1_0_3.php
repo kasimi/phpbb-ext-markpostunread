a7bf962bd512d5d7c09d469b960a13d907562ffe
dmzx <info@dmzx-web.net>
2016-03-11 09:04:54 +0100

    Add Permission

---
+++ kasimi/markpostunread/migrations/v1_0_3.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *
+ * @package phpBB Extension - Mark Post Unread
+ * @copyright (c) 2015 kasimi
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace kasimi\markpostunread\migrations;
+
+class v1_0_3 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\kasimi\markpostunread\migrations\v1_0_2');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.update', array('kasimi.markpostunread.version', '1.0.3')),
+
+			// Add permissions
+			array('permission.add', array('u_markpostunread_use', true)),
+
+			// Set permissions
+			array('permission.permission_set', array('ADMINISTRATORS', 'u_markpostunread_use', 'group')),
+		);
+	}
+}
